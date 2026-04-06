9e59d0a3c1872e03eb911164cd874107711c02fd
linqingquan <492332435@qq.com>
2026-04-06 22:46:44 +0800

    修复单元测试：JUnit 4 升级到 JUnit 5

--- index-gather-store-service/src/test/java/cn/how2j/trend/AppTest.java
+++ index-gather-store-service/src/test/java/cn/how2j/trend/AppTest.java
@@ -1,8 +1,8 @@
 package cn.how2j.trend;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
